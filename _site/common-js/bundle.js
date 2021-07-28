3feb34550de45d3eaa31725f52d465bd3a5f59d7
scribblerp9 <szappia86@gmail.com>
2021-07-28 22:23:03 +1000

    Deleting _site

--- _site/common-js/bundle.js
+++
@@ -1,7 +0,0 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// Adding to the window object makes these modules available elsewhere, including inside the Alpine component!
-// Global variables are generally frowned upon of course, but this seems to be the best viable solution
-// e.g:
-// window.simpleEncryptor = require('simple-encryptor')('this is a key that should be replaced by an ENV variable');
-
-},{}]},{},[1]);
